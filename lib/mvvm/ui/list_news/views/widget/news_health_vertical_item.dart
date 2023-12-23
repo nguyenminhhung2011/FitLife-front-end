fa274e3a8ec216fd83c686134b191c79ca2478bc
Nguyễn Minh Hưng <90996598+minhunsocute@users.noreply.github.com>
2023-12-23 15:15:04 +0700

    💥chore(news): navigate to detail from news item

--- lib/mvvm/ui/list_news/views/widget/news_health_vertical_item.dart
+++ lib/mvvm/ui/list_news/views/widget/news_health_vertical_item.dart
@@ -1,8 +1,10 @@
+import 'package:fit_life/app_coordinator.dart';
 import 'package:fit_life/core/components/constant/handle_time.dart';
 import 'package:fit_life/core/components/extensions/context_extensions.dart';
 import 'package:fit_life/core/components/layout/setting_layout/controller/setting_bloc.dart';
 import 'package:fit_life/core/components/widgets/image_custom.dart';
 import 'package:fit_life/mvvm/me/entity/news_health/news_health.dart';
+import 'package:fit_life/routes/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -23,97 +25,103 @@ class _NewsHealthVerticalItemState extends State<NewsHealthVerticalItem> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          width: double.infinity,
-          margin: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15.0),
-            boxShadow: [
-              BoxShadow(
-                blurRadius: 5.0,
-                color: Theme.of(context).shadowColor.withOpacity(0.1),
-              )
-            ],
-            color: Theme.of(context).cardColor,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: double.infinity,
-                height: 150,
-                child: ClipRRect(
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(15.0),
-                    topRight: Radius.circular(15.0),
+    return GestureDetector(
+      onTap: () =>
+          context.openPageWithRouteAndParams(Routes.newsDetail, widget.item.id),
+      child: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            margin:
+                const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15.0),
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 5.0,
+                  color: Theme.of(context).shadowColor.withOpacity(0.1),
+                )
+              ],
+              color: Theme.of(context).cardColor,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  height: 150,
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(15.0),
+                      topRight: Radius.circular(15.0),
+                    ),
+                    child: ImageCustom(
+                        imageUrl: widget.item.urlToImage,
+                        isNetworkImage: true,
+                        width: double.infinity),
                   ),
-                  child: ImageCustom(
-                      imageUrl: widget.item.urlToImage,
-                      isNetworkImage: true,
-                      width: double.infinity),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Expanded(
-                          child: Text(
-                            widget.item.author.isEmpty
-                                ? "Empty author"
-                                : widget.item.author,
-                            style: context.titleMedium
-                                .copyWith(fontWeight: FontWeight.bold),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            child: Text(
+                              widget.item.author.isEmpty
+                                  ? "Empty author"
+                                  : widget.item.author,
+                              style: context.titleMedium
+                                  .copyWith(fontWeight: FontWeight.bold),
+                            ),
                           ),
-                        ),
-                        Text(
-                          getYmdFormat(widget.item.publishedAt),
-                          style: context.titleSmall
-                              .copyWith(fontWeight: FontWeight.w600),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 2),
-                    Text(
-                      widget.item.description,
-                      style: context.titleSmall
-                          .copyWith(color: Theme.of(context).hintColor),
-                    ),
-                  ],
+                          Text(
+                            getYmdFormat(widget.item.publishedAt),
+                            style: context.titleSmall
+                                .copyWith(fontWeight: FontWeight.w600),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 2),
+                      Text(
+                        widget.item.description,
+                        style: context.titleSmall
+                            .copyWith(color: Theme.of(context).hintColor),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-        BlocBuilder<SettingBloc, SettingState>(
-          bloc: _bloc,
-          builder: (_, state) {
-            final favoritesNews =
-                state.data.currentUser?.userProfile?.favoriteNews ??
-                    List.empty(growable: true);
-            final isFavorite = favoritesNews
-                .where((element) => element.id == widget.item.id)
-                .isNotEmpty;
-            final icon = isFavorite
-                ? Icon(Icons.favorite, color: Colors.red[400])
-                : Icon(Icons.favorite_border, color: context.titleLarge.color);
-            return Positioned(
-              top: 5.0,
-              right: 15.0,
-              child: IconButton(
-                  onPressed: () =>
-                      _bloc.add(SettingEvent.addFavoriteNews(widget.item)),
-                  icon: icon),
-            );
-          },
-        )
-      ],
+          BlocBuilder<SettingBloc, SettingState>(
+            bloc: _bloc,
+            builder: (_, state) {
+              final favoritesNews =
+                  state.data.currentUser?.userProfile?.favoriteNews ??
+                      List.empty(growable: true);
+              final isFavorite = favoritesNews
+                  .where((element) => element.id == widget.item.id)
+                  .isNotEmpty;
+              final icon = isFavorite
+                  ? Icon(Icons.favorite, color: Colors.red[400])
+                  : Icon(Icons.favorite_border,
+                      color: context.titleLarge.color);
+              return Positioned(
+                top: 5.0,
+                right: 15.0,
+                child: IconButton(
+                    onPressed: () =>
+                        _bloc.add(SettingEvent.addFavoriteNews(widget.item)),
+                    icon: icon),
+              );
+            },
+          )
+        ],
+      ),
     );
   }
 }
